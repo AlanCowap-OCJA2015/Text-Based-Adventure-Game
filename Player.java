8ff08802f5a7d78c6cb1250b77e91518be6853c7
Peter Clarke <peterclarkedublin@live.com>
2015-09-10 11:48:06 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -11,10 +11,10 @@ public class Player{
 		this.playerType = playerType;
 
 		switch(playerType){
-		case Warrior : this.playerType = "Warrior".this.hp = 100.this.energy = 50.this.luck = 50; break;
-		case Mage : this.playerType = "Mage".this.hp = 80.this.energy = 80.this.luck = 75; break;
-		case Ninja : this.playerType = "Ninja".this.hp = 90.this.energy = 100.this.luck = 50; break;
-		case Athlete : this.playerType = "Athlete".this.hp = 80.this.energy = 100.this.luck = 50; break;
+		case "Warrior" : this.playerType = "Warrior".this.hp = 100.this.energy = 50.this.luck = 50; break;
+		case "Mage" : this.playerType = "Mage".this.hp = 80.this.energy = 80.this.luck = 75; break;
+		case "Ninja" : this.playerType = "Ninja".this.hp = 90.this.energy = 100.this.luck = 50; break;
+		case "Athlete" : this.playerType = "Athlete".this.hp = 80.this.energy = 100.this.luck = 50; break;
 		default : this.playerType = "Wanderer".this.hp = 50.this.energy = 50.this.luck = 50; break;
 
 		}
@@ -24,3 +24,5 @@ public class Player{
 
 
 }
+
+public
