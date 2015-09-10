34d233ea3b5f45214ca44f5f4a999b2bcf9698bb
Artjoms Porss <artjoms.porss@gmail.com>
2015-09-10 13:39:33 +0100

    researching how to get input from user
    and move through dialogue branches

--- JourneyOfAChampion.java
+++ JourneyOfAChampion.java
@@ -1,6 +1,9 @@
 //main class which loads the game
 
+import java.util.Scanner;
+
 public class JourneyOfAChampion {
+	private Scanner scan = new Scanner(System.in);
 
 	public static void main( String [] ags ) {
 		JourneyOfAChampion joac = new JourneyOfAChampion();
@@ -15,13 +18,33 @@ public class JourneyOfAChampion {
 		System.out.println("\t" + player.toString());
 
 
-		//Loads the file and displays it
+		//Loads the file
 		FileLoader fl = new FileLoader("C:/java/CodeChallengeGame/Dialogues.txt");
-		System.out.println(fl.toString());
+//		System.out.println(fl.toString());	//TEST display of text loaded
 
-		//test with dialogue
+		//test dialogue
 		Dialogue dialogue = new Dialogue(fl.toString() , player.getName(), "Stranger");
-		System.out.println(dialogue.getOption("5"));
+		System.out.println(dialogue.getNextMessage());
+		
+		//get user choice
+		System.out.print("Choose an option> ");
+		String inputStr = scan.nextLine();
+		int inputInt = 0;
+		try{
+			inputInt = Integer.parseInt(inputStr);
+		}catch(NumberFormatException nfe){
+			System.out.println("Please choose a valid option..");
+		}
+		
+		//check if chosen option is one of the available in this dialogue branch
+		if(inputInt > 0 && inputInt <= dialogue.getOptionCount()){
+			System.out.println("This is a valid option..");
+		} else {
+			System.out.println("There is no such option..");
+		}
+		
+		
+
 	}
 
 }
