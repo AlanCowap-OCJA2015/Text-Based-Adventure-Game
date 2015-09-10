71553ae68105ca22df8725e8a611d88056539e4c
Artjoms Porss <artjoms.porss@gmail.com>
2015-09-10 15:46:57 +0100

    need to work on dialogue ending
    Dialogue ends abruptly and doesn't show the last message before end

--- Dialogue.java
+++ Dialogue.java
@@ -1,6 +1,7 @@
 //holds dialogue text between you and someone
 //allows to move through dialogue branching until dilagoue ends
 
+
 class Dialogue {
 	private String text;	//all dialogue text
 	private String plName;	//players name
@@ -15,26 +16,42 @@ class Dialogue {
 		this.somName = somName;
 	}
 
-	//goes to next line specified in parameters and returns it
+
+	//goes to next question line
+	//generates answer list available for that question
+	//returns generated string
 	public String getNextMessage(){
 
 		String option = ""+ dialogPosition;
-
 		String displayText = "";		
-
 		int startPos = text.indexOf(option + "<") + option.length();
 
-		//System.out.println(option);	//TEST
-		//System.out.println(startPos);//TEST
+	//	System.out.println(option);	//TEST
+	//	System.out.println(startPos);//TEST
 
+		//gets ¬ character position after the question
 		int endPos = text.indexOf("¬", startPos);
 
 		//System.out.println(endPos);//TEST
 
+		//gets position of \n character after ¬ character
+		//will be needed to determine what option chosen branch moves to
+		int newLinePos = text.indexOf("\n", endPos);
+
+		String endString = text.substring(endPos, newLinePos);
+		//System.out.println("-" + endString + "-");//TEST
+		
+		if(text.substring(endPos, newLinePos).equals("¬END")){
+			System.out.println("Dialogue ends");
+			return null;
+		}
+
 		displayText = "\n" + text.substring(startPos, endPos);
 
 
+
 		//GENERATE AVAILABLE OPTIONS
+		optionCount = 0;
 		boolean hasOptions = true;
 		while(hasOptions){
 			String currentOption = option + "." + (1 + optionCount);
@@ -53,7 +70,64 @@ class Dialogue {
 	}
 
 
-	
+
+//accepts user picked option
+//displays appropriate message
+//with respond from someone
+//and another subset of options available
+	public String pickOption(int userOption){
+
+		if(!(userOption > 0 && userOption <= optionCount)){
+			return null;
+		}
+
+		/*
+1<Hello Stranger! - 'You see a man on crutches, he has no left leg'.¬
+	1.1>Hello, what do you want?¬2
+	1.2>I have no time for you, get out of my way!¬3
+	1.3>Sorry, I have no time, see you later!¬4
+	1.4>Say nothing, go away¬END
+2<Please bring this letter to my friend, I am disabled and can't walk..¬
+	2.1>Okay - 'Take the letter'.¬5
+	2.2>Maybe next time¬END
+3<Idiot! - 'Smacks you with a crutch'.¬END
+4<Please, I need your help!¬
+	4.1>Okay, what do you want?¬2
+5<Thank you! Goodbye!¬END
+		*/
+		
+		//gets position of chosen option
+		String optionStr = dialogPosition + "." + userOption + ">";
+		int optionPos = text.indexOf(optionStr);
+		//System.out.println(optionPos);	//TEST
+
+		//gets position of ¬ caracter after chosen option
+		int movePos = text.indexOf("¬", optionPos);
+		//System.out.println(movePos);	//TEST
+
+		//gets position of \n character after ¬ character
+		//will be needed to determine what option chosen branch moves to
+		int newLinePos = text.indexOf("\n", movePos);
+		//System.out.println(newLinePos);	//TEST
+		
+		String displayText = "\nYou say: " + text.substring(optionPos + optionStr.length(), movePos);
+		//System.out.println("\nYou say: " + displayText;)	//TEST
+
+
+		//determining where branching goes to
+		String moveOptionStr = text.substring(movePos + 1, newLinePos);
+		//System.out.println("-"+moveOptionStr+ "-");	//TEST
+		try{
+			dialogPosition = Integer.parseInt(moveOptionStr);
+		} catch (NumberFormatException nfe){
+			//nfe.printStackTrace();
+			System.out.println("Dialogue ends");
+			return null;	//dialog ends at the end of option you choose
+		}
+		//System.out.println("-" + dialogPosition + "-");	//TEST
+
+		return displayText;
+	}
 
 
 	public int getOptionCount(){
