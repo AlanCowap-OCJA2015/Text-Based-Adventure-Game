ecbd1c7e9be6d5d22354f18a5db8fd7bd09a84ba
JennQuinn42 <jennquinn42@gmail.com>
2015-09-10 14:31:57 +0100

    Update GameApp.java

--- GameApp.java
+++ GameApp.java
@@ -1,71 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Game{
+public class GameApp{
 
-	private ArrayList<Question> path = new ArrayList<Question>();
-	private ArrayList<Life> encounters = new ArrayList<Life>();
-
-	public Game(String fileName){
-		GetAdventure adventure = new GetAdventure();
-		path = adventure.readFile(fileName);
-	}
-
-	public void playGame(){
-
-		chooseMove(path.get(0));
-
-	}
-
-	public void chooseMove(Question quest){
-		if(quest.getOpt1() > 0){
-			Scanner scan = new Scanner(System.in);
-			System.out.println(quest.getQuestion());
-			System.out.print("Which option do you pick?");
-			validateInput(scan);
-			int choice = scan.nextInt();
-
-			switch(choice){
-				case 1: chooseMove(path.get(quest.getOpt1())); break;
-				case 2: chooseMove(path.get(quest.getOpt2())); break;
-				default: break;
-			}
-		}
-		else{
-			System.out.println(quest.getQuestion());
-			System.out.println("Congratulation, you have completed your quest, "
-				+ " and killed all enemies. Live long and prosper!");
-			System.exit(0);
-		}
-	}
-
-	public void generateEncounters(int numEncounters){
-		Scanner scanEncounter = new Scanner(System.in);
-		for(int i = 0; i < numEncounters; ++i){
-			System.out.println("What is the type of encounter?");
-			String type = scanEncounter.next();
-			System.out.println("How much health should " + type + " have?");
-			validateInput(scanEncounter);
-			int health = scanEncounter.nextInt();
-			System.out.println("How much attack power should " + type + " have?");
-			validateInput(scanEncounter);
-			int attack = scanEncounter.nextInt();
-			System.out.println("How likely is " + type + " to appear?");
-			validateInput(scanEncounter);
-			int luck = scanEncounter.nextInt();
+	public static void main(String[] args){
+		System.out.println("Welcome to Adventure Times's \"Your Time is now!\"");
 			
-			encounters.add(new Life(type, health, attack, luck));
-			
-		}
-		System.out.println("");
-		
-	}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter the name of the adventure (file) you wish to play?" 
+				+ "\nLeave blank for default adventure");
+		String fileName = scan.nextLine();
+		//scan.close();
+		Game fun = new Game(fileName);
 
-	private void validateInput(Scanner scan){
+		System.out.println("How many enemies would you like to encounter?");
 		while(!scan.hasNextInt()){
 			scan.next();
-			System.out.println("Not a valid option please enter either 1 or 2:");
+			System.out.println("Not a valid number, please try again");
 		}
+		int numEncounters = scan.nextInt();
+		fun.generateEncounters(numEncounters);
+		fun.playGame();
 	}
 
 }
