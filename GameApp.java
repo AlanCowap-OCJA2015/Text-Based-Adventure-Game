741098e82662d7aedb4758cf885f53989f5bead9
JennQuinn42 <jennquinn42@gmail.com>
2015-09-10 10:34:02 +0100

    Update GameApp.java
    Updated GameApp to test GetAdventure class

--- GameApp.java
+++ GameApp.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class GameApp{
 
 	public static void main(String[] args){
 		System.out.println("Hello Choose your own adventure");
-
-		String str = "What need have you#But come to sense#To fumble in the greasy till#And add the halfpence to the pence";
-		Scanner scan = new Scanner(str);
-		scan.useDelimiter("#");
-		while(scan.hasNext()){
-			System.out.println(scan.next());
-		}		
+			
+		GetAdventure adventure = new GetAdventure();
+		ArrayList<String> choices = null;
+		choices = adventure.readFile();
+		for(String s:choices){
+			System.out.println(s);
+		}
 	}
 
 }
